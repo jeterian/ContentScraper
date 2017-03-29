68e3c351fc839ac56e94271dcb4f48fe5297bea3
jeterian <jeterian@gmail.com>
2017-03-29 17:10:58 -0400

    first pass through

--- scraper.js
+++ scraper.js
@@ -9,15 +9,14 @@ const scraper = require('x-ray'); // content scraper
 const xray = scraper();
 const writeCSV = require('csv-write-stream'); // parses json to csv
 const parse = writeCSV();
- // for handling the date
-var moment = require('moment');
-moment().format();
+const moment = require('moment'); // for handling the date
+const request = require('request');
 const url = 'http://shirts4mike.com/shirts.php';
 const folder = './data';
 const date = moment().format("YYYY-MM-DD");
 
-// 1. Program scraper to check for folder named 'Data'
-var dataFolder = new Promise(function(resolve, reject) {
+// 1. Check for folder named 'Data'/ Check and see if URL works
+var checkURL = function(resolve, reject) {
 
   // If 'Data' doesn't exist, create it
   if (!fs.existsSync(folder)) {
@@ -25,23 +24,50 @@ var dataFolder = new Promise(function(resolve, reject) {
     fs.mkdirSync(folder);
     writer.pipe(fs.createWriteStream(folder + date + '.csv'));
   }
-});
+
+  request(url, (error, response, body) =>  {
+    if(!error && response.statusCode === 200) {
+      resolve(body);
+    } else {
+      reject(error);
+    }
+  })
+};
 
 // 2. Use npm package that scrapes content from a site.  Retrieve price, title,
 //    url and image url from product page and save as CSV
+  xray(url, '.products li', [{
+    title: xray('a@href', 'title'),
+    price: xray('a@href', 'price'),
+    title: 'img@src',
+    url: 'a@href'
+  }])
 
-  // CSV named for date
+    // EC: Log to a file named scraper-error.log that appends to bottom wih timestamp
+    ((error, object) => {
+      if(error) {
+        const logErr = date + ': ' + error;
+        console.log(logErr);
+        fs.appendFileSync('scraper-error.log', logErr + '\n');
+        if (error.errno === 'ENOTFOUND') {
+          const fourOfour = '404';
+          console.log(`404 Error: Cannot connect to ${url}.`);
+        } else if (error.errno ==='ETIMEDOUT'){
+            console.log(`Timeout Error: Cannot connect to ${url}.`);
+        } else {
+          console.log(`${error.name} error. Cannot connect to ${url}.`)
+        }
+      } else {
+        for (key in object) {
+          object[key].time = date;
+        }
 
-  // Column headings in the following order: title, price, imageurl, url, time
+// Column headings in the following order: title, price, imageurl, url, time
+        const headers = ['title', 'price', 'imgUrl', 'url', 'time'];
+        const csv = parse({data: object, fields: headers});
+        console.log(csv);
 
-  // Saved in 'Data' folder
-
-// 6. If program is run twice, it should overwrite "Data"
-
-// 7. If http://shirts4mike.com is down, give error message
-
-  // EC: Log to a file named scraper-error.log that appends to bottom wih timestamp
-
-// 8. Include package.json with dependencies and npm install should install dependencies
-
-  // EC: edit package.json file so it runs when npm start is run
+          // CSV named for date
+        fs.writeFileSync(folder + date + ".csv", csv);
+      }
+    });
