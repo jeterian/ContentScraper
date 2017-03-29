bdce0769473d400ed41a178923add2fd9386dbce
jeterian <jeterian@gmail.com>
2017-03-29 19:44:28 -0400

    Final updates

--- scraper.js
+++ scraper.js
@@ -1,73 +1,60 @@
-// Problem: Need a content scraper that takes information from
-// http://shirts4mike.com and puts the information in a CSV File
+// Treehouse Project 6 - Content scraper
 
-'use strict';
-
-// initial variables for fs, 3d party npm packages
+// variables for npm modules and other essentials
 const fs = require('fs');
-const scraper = require('x-ray'); // content scraper
-const xray = scraper();
-const writeCSV = require('csv-write-stream'); // parses json to csv
-const parse = writeCSV();
-const moment = require('moment'); // for handling the date
-const request = require('request');
+const xray = require('x-ray');
+const json2csv = require('json2csv');
+const moment = require('moment');
+var x = new xray();
 const url = 'http://shirts4mike.com/shirts.php';
-const folder = './data';
-const date = moment().format("YYYY-MM-DD");
+const products = '.products li';
 
-// 1. Check for folder named 'Data'/ Check and see if URL works
-var checkURL = function(resolve, reject) {
+// 1. Test to see if Data folder exists, if not, create one.
+if (!fs.existsSync('./data/')){
+    console.log('Creating folder...')
+    fs.mkdir('.//data', function (error) {
+      // if something goes wrong, error logged to log file
+        if (error) {
+            const logErrs = moment().format + ' - ' + error;
+            console.log(logErrs);
+            fs.appendFileSync('scraper-error.log', logErrs + '\n');
+        };
+    })
+}
 
-  // If 'Data' doesn't exist, create it
-  if (!fs.existsSync(folder)) {
-    console.log("Creating Data folder...");
-    fs.mkdirSync(folder);
-    writer.pipe(fs.createWriteStream(folder + date + '.csv'));
-  }
+// 2. Scrape the site to retrieve the information requested.
 
-  request(url, (error, response, body) =>  {
-    if(!error && response.statusCode === 200) {
-      resolve(body);
-    } else {
-      reject(error);
-    }
-  })
-};
-
-// 2. Use npm package that scrapes content from a site.  Retrieve price, title,
-//    url and image url from product page and save as CSV
-  xray(url, '.products li', [{
-    title: xray('a@href', 'title'),
-    price: xray('a@href', 'price'),
-    title: 'img@src',
-    url: 'a@href'
-  }])
-
-    // EC: Log to a file named scraper-error.log that appends to bottom wih timestamp
+x(url, products,  [{
+    title : x('a@href', 'title'),
+    price : x('a@href', '.price'),
+    imgUrl : 'img@src',
+    url : 'a@href'
+}])
     ((error, object) => {
-      if(error) {
-        const logErr = date + ': ' + error;
-        console.log(logErr);
-        fs.appendFileSync('scraper-error.log', logErr + '\n');
-        if (error.errno === 'ENOTFOUND') {
-          const fourOfour = '404';
-          console.log(`404 Error: Cannot connect to ${url}.`);
-        } else if (error.errno ==='ETIMEDOUT'){
-            console.log(`Timeout Error: Cannot connect to ${url}.`);
-        } else {
-          console.log(`${error.name} error. Cannot connect to ${url}.`)
+        if(error) {
+              // EC: Log to a file named scraper-error.log that appends to bottom wih timestamp
+            const logErrs = moment().format() + ' - ' + error;
+            console.log(logErrs);
+            fs.appendFileSync('scraper-error.log', logErrs + '\n');
+            if (error.errno === 'ENOTFOUND') {
+                console.log(`A 404 error has occured.`);
+            } else if (error.errno === 'ETIMEDOUT'){
+                console.log(`A timeout error has occured.`);
+            } else {
+                console.log(`A ${error.errno} error has occured.`);
+            }
         }
-      } else {
-        for (key in object) {
-          object[key].time = date;
+        else {
+            // append time
+            const now = moment().format('YYYY-MM-DD');
+            for (key in object){
+                object[key].time = now;
+            }
+
+            // save csv
+            const headings = ['title', 'price', 'imgUrl', 'url', 'time'];
+            const csv = json2csv({ data: object, fields: headings});
+            console.log(csv);
+            fs.writeFileSync(`./data/${now}.csv`, csv);
         }
-
-// Column headings in the following order: title, price, imageurl, url, time
-        const headers = ['title', 'price', 'imgUrl', 'url', 'time'];
-        const csv = parse({data: object, fields: headers});
-        console.log(csv);
-
-          // CSV named for date
-        fs.writeFileSync(folder + date + ".csv", csv);
-      }
     });
